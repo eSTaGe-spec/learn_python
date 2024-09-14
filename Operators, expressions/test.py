de85c7de89ade02bc21cf250db6d3f915f9040bf
Joseph <boss.norotov@list.ru>
2024-09-14 22:53:58 +0300

    commit

---
+++ Operators, expressions/test.py
@@ -0,0 +1,22 @@
+import unittest
+from josephNoob import chance_to_kill
+from volleyball_legend import volleyball_legend
+from pay_for_win import pay_pls
+
+
+# class TestL1(unittest.TestCase):
+#     def test_lesson_1(self):
+#         self.assertEqual(chance_to_kill(1, 70), 7)
+#         self.assertEqual(chance_to_kill(2, 70), 8)
+#         self.assertEqual(chance_to_kill(5, 70), 10)
+#
+#
+# class TestL2(unittest.TestCase):
+#     def test_lesson_1(self):
+#         self.assertEqual(volleyball_legend(3, 2), 80)
+
+
+class TestL3(unittest.TestCase):
+    def test_lesson_1(self):
+        self.assertEqual(pay_pls(500, -50), 275)
+
